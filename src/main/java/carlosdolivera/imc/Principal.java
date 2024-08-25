218d0b006e328d06c389ede29fe237d5b4f6fc36
CodgPro <carlosdavidoliverag@gmail.com>
2024-08-24 19:21:38 -0500

    Firt commit - Aplicacion servidor imc

---
+++ src/main/java/carlosdolivera/imc/Principal.java
@@ -0,0 +1,11 @@
+package carlosdolivera.imc;
+
+import carlosdolivera.imc.vistas.VentanaPrincipal;
+
+public class Principal {
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+}
